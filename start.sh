51fd1924d02307dc48ca33c26617017c7311b437
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-02 14:24:22 +0300

    fix bug

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 export MAIL_USERNAME='antonymunyasia993@gmail.com'
 export MAIL_PASSWORD='tollysmith993.'
 export SECRET_KEY='stuxnet993..'
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/perfectpitch'
 
 
 
