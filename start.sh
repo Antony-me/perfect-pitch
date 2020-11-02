45a321b3abca222b6f5e3a867dcc1451d6ade00d
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-02 09:53:34 +0300

    Production

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-# export SECRET_KEY='stuxnet993..'
+export MAIL_USERNAME='antonysimiyu993@gmail.com'
+export MAIL_PASSWORD='tollysmith993..'
+export SECRET_KEY='stuxnet993..'
 
 python3.6 manage.py server
